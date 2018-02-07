030bd63588f5dd84598e7f952cc13ac4aef077dc
scrdest <jan.k.malek@gmail.com>
2018-02-07 14:34:23 +0100

    Adds a text UI/menu. Adds support for prediction, refactors picking labels to random (but paired between test/train data) sampling, and probably more.

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import dparser as geo
 import pandas as pd
 import numpy as np
 import itertools as itt
-import random
+import collections as coll
 #from pprint import pprint as print
 
 def build_models(datashape, compression_fac=32, activators=('relu', 'sigmoid'), **kwargs):
@@ -62,6 +62,7 @@ def fetch_batch(stream, batch_size=10, aggregator=list, *args, **kwargs):
                                  args and kwargs are passed as the arguments of the call. Default: list
     """
     bsize = batch_size
+    batch = aggregator(*args, **kwargs)
     while stream:
         batch = aggregator(*args, **kwargs)
         for _ in range(batch_size):
@@ -76,8 +77,10 @@ def split_data(batches, test_to_train=0.2, shuffle=True):
     for raw_data in batches:
         if not raw_data: continue
         data = raw_data.copy()
-        if shuffle: random.shuffle(data)
-        #data = tuple(map(lambda x: pd.DataFrame.from_dict(x, orient='index'), data))
+        if shuffle: 
+            from random import shuffle as randshuffle
+            randshuffle(data)
+        #data = (map(lambda x: pd.DataFrame.from_dict(x, orient='index'), data))
         
         # split data:
         batch_size = len(data)
@@ -96,8 +99,21 @@ def split_data(batches, test_to_train=0.2, shuffle=True):
 
         yield train
         yield test
-    
-def main(verbose=None, *args, xml=None, txt=None, dir=None, **kwargs):
+                   
+def load_model(*args, model_path=None, **kwargs):
+    model, loaded_path = None, None
+    if not model_path: model_path = input("Path to load:\n>>> ")
+    try:
+        import keras
+        model = keras.models.load_model(model_path)
+        loaded_path = model_path
+    except Exception as Err:
+        if kwargs.get('verbose'): sys.excepthook(*sys.exc_info())
+        print("\n\nModel could not be loaded from path {}!".format(loaded_path))
+    return model, loaded_path
+          
+def build_datastreams_gen(xml=None, txt=None, dir=None, **kwargs):
+    DEBUG = kwargs.get('debug', False)
     dir = os.getcwd() if not dir else (dir if os.path.isabs(dir) else os.path.join(os.getcwd(), dir))
     xml = xml or os.path.join(dir, '*.xml')
     txt = txt or os.path.join(dir, '*.txt')
@@ -118,32 +134,31 @@ def main(verbose=None, *args, xml=None, txt=None, dir=None, **kwargs):
     
     # load values for each accession:
     def get_file_data(f):
-        return next(geo.txt_pipeline(os.path.join(dir, f + '*'))).tail(10000)
+        return next(geo.txt_pipeline(os.path.join(dir, f + '*')))
         
     train = (tuple(zip(x, map(get_file_data, x.values()))) for x in train_files)
     test = (tuple(zip(x, map(get_file_data, x.values()))) for x in test_files)
+    # print ( ( (next(train)) ) )
+    # return
     
     drop_label = True
     retrieve_df = ((lambda x: x[1]) if drop_label 
                     else (lambda pair: pair[1].assign(Label=np.array(pair[0]))))
-    train = (tuple(map(retrieve_df, df)) for df in train)
-    test = (tuple(map(retrieve_df, df)) for df in test)
+    train = ((map(retrieve_df, df)) for df in train)
+    test = ((map(retrieve_df, df)) for df in test)
+    
+    nxt = next(itt.chain.from_iterable(train))
+    nxt = nxt.sample(10000)
+    train_size = nxt.shape # sample the data for size
+    labels = nxt.index.values
+    if kwargs.get('verbose'): print('LABELS PICKED: ', labels, '\n')
+    
+    # drop everything not picked in this sampling
+    train = (itt.chain.from_iterable(map(lambda x: x.loc[labels], df) for df in train))
+    test = (itt.chain.from_iterable(map(lambda x: x.loc[labels], df) for df in test))
+    train = itt.chain((nxt,), train) # return the sampled column for processing
     #print ( ( (next(train)) ) )
-    #return
-    
-    # sample the data for size:
-    train_size = min(x.shape for x in next(train))
-    test_size = min(x.shape for x in next(test))
-    if train_size != test_size:
-        train_size = min(train_size, test_size)
-        raise RuntimeWarning('Unequal sizes of test and train data, assuming the minimal value!')
-        
-    # Split the data up into single datapoints
-    
-    train = itt.chain.from_iterable(train) #break it up into individual records
-    test = itt.chain.from_iterable(test)
-    #print(next(train), '\n'*3, next(test))
-    #return
+    #return (train,)
 
     # train = ((x.T.values, np.array([x.index.values])) for x in train)
     # test = ((x.T.values, np.array([x.index.values])) for x in test)
@@ -151,46 +166,229 @@ def main(verbose=None, *args, xml=None, txt=None, dir=None, **kwargs):
     test = ((x.T.values) for x in test)
     #print(next(train), '\n'*3, next(test))
     #return
-    traing = (zip(train, test))
+    #traing = (zip(train, test))
+    size = train_size
+    mode = kwargs.get('mode', 'train')
+    datagen = {'train': lambda: zip(train, train), 
+            'test': lambda: zip(test, test), 
+            'cross': lambda: zip(train, test),}.get(mode, lambda: None)()
+    return datagen, size
+          
+# rename this!
+def run_model(models=None, verbose=None, *args, xml=None, txt=None, dir=None, **kwargs):
     
-    models = build_models(datashape=train_size, depth=4)
-    #return
+    mode = kwargs.get('mode', 'train')
+    datagen, size = build_datastreams_gen(xml=xml, txt=txt, dir=dir, mode=mode)
+    mode_func = {'train' : lambda x: x[0].fit_generator(datagen, steps_per_epoch=75),
+                 'test' : lambda x: x[1].predict_generator(datagen, steps=75, verbose=1),
+                }.get(mode)
+    
+    built_models = [None]
+    if not all(models): built_models = build_models(datashape=size, depth=kwargs.get('depth', 2))
+    models = [x or built_models[i] for (i,x) in enumerate(models)]
     autoencoder = models[0]
     autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
-    def pop_and_put(gen):
-        nxt = next(gen) # pop...
-        return (nxt, itt.chain([nxt], gen)) #...and return for processing
+    
     fits = 1
     mdl_file = NotImplemented
+    result = None
     while fits:
         fits -= 1
         if not fits or fits < 1:
             while True:
                 try:
-                    fits = int(input("Enter a number of fittings to perform before prompting again.\n (values <= 0 will terminate the program): "))
+                    fits = int(input("Enter a number of fittings to perform before prompting again.\n (value <= 0 to terminate): "))
                     break
+                except (KeyboardInterrupt, EOFError):
+                    return
                 except Exception as Exc:
-                    if True: print(Exc)
+                    if verbose: print(Exc)
                     print("Invalid input. Try again.")
                     
         if fits:
             if mdl_file is NotImplemented: mdl_file = input("If you want to save the model, enter the filename of file to save it to: ")
             
-            try: autoencoder.fit_generator(traing, steps_per_epoch=75)#, initial_epoch=fits+1)
+            try: result = mode_func(models)
             except KeyboardInterrupt: fits = 0
             
             if mdl_file and (not fits % 10 or 0 <= fits < 10): 
                 try: os.replace(mdl_file, mdl_file+'.backup')
                 except Exception: pass
                 autoencoder.save(mdl_file)
+                
+        else: mdl_file = None if mdl_file is NotImplemented else mdl_file
+    
+    return (models, result, mdl_file)
 
+class MenuConfiguration(object):
+    options = coll.OrderedDict()
+        
+ACT_TRAIN= '(T)rain'
+ACT_PRED = '(P)redict'
+ACT_LOAD = '(L)oad model'
+ACT_SAVE = '(S)ave model'
+ACT_DROP = '(D)rop model'
+ACT_CONF = '(C)onfigure'
+ACT_QUIT = '(Q)uit'
+
+DBG_DATA = 'DEBUG DATA (!)'
+    
+def main(verbose=None, *args, xml=None, txt=None, dir=None, **kwargs):
+    modes = coll.OrderedDict()
+    
+    modes.update({ACT_TRAIN: {'1', 'train', 't'},})
+    modes.update({ACT_PRED : {'2', 'predict', 'p'},})
+    modes.update({ACT_LOAD : {'3', 'load', 'l'},})
+    modes.update({ACT_SAVE : {'4', 'save', 's'},})
+    modes.update({ACT_DROP : {'5', 'drop', 'd'},})
+    modes.update({ACT_CONF : {'6', 'configure', 'c'},})
+    modes.update({ACT_QUIT : {'0', 'quit', 'q'},})
+    
+    modes.update({DBG_DATA : {'!', '-1'},})
+    
+    mainloop = True
+    action = NotImplemented
+    model = [None, None, None]
+    prediction, history, modelpath = None, None, None
+    config = MenuConfiguration
+    config.options.update(sorted([('verbosity', verbose), ('xml_path', xml), ('txt_path', txt), ('directory', dir)]))
+    config.options.update(kwargs)
+    
+    baseprompt = """
+           __________
+          /          \\
+          |   MENU   |
+          \__________/
+ {mdl}
+  
+ The available options are: 
+   - {opts}.
+    
+>>> """.format(opts=',\n   - '.join(modes), mdl="{mdl}")
+    
+    while mainloop:
+        prompt = baseprompt.format(mdl=(('\n Currently loaded model: '+ str(modelpath))))
+        if not action: 
+            prompt = '>>> '
+            action = NotImplemented
+        try:
+            action = input(prompt).lower()
+        except (KeyboardInterrupt, EOFError):
+            action = None
+            mainloop = False
+        
+        if action in modes[ACT_TRAIN]: 
+            _tmp = run_model(*args, models=model, verbose=verbose, xml=xml, txt=txt, dir=dir, mode='train', **kwargs)
+            
+            try: _tmpFN = _tmp[2]
+            except Exception as Err: _tmpFN = None
+            
+            try: history = _tmp[1]
+            except Exception as Err: history = None
+            
+            try: _tmp = _tmp[0]
+            except Exception as Err: _tmp = None
+            
+            model = _tmp if _tmp else model
+            modelpath = _tmpFN if (_tmp and _tmpFN) else (str(model) if _tmp else modelpath)
+            
+        if action in modes[ACT_PRED]:
+            _tmp = run_model(*args, models=model, verbose=verbose, xml=xml, txt=txt, dir=dir, mode='test', **kwargs)
+            
+            try: _tmpFN = _tmp[2]
+            except Exception as Err: _tmpFN = None
+            
+            try: prediction = _tmp[1]
+            except Exception as Err: prediction = None
+            
+            try: _tmp = _tmp[0]
+            except Exception as Err: _tmp = None
+            
+            model = _tmp if _tmp else model
+            modelpath = _tmpFN if (_tmp and _tmpFN) else (str(model) if _tmp else modelpath)
+            if prediction is not None: print(prediction)
+                
+            
+        if action in modes[ACT_LOAD]:
+            try: _tmp, _tmp2 = load_model()
+            except Exception as Err: _tmp = None
+            if _tmp: 
+                model = list(model)
+                model[0] = _tmp
+                modelpath = str(_tmp2)
+                print("Model loaded successfully.")
+        
+        if action in modes[ACT_SAVE]:
+            savepath = input("Enter the filename of file to save it to: ")
+            savepath = savepath.strip() if savepath else savepath
+            if savepath: 
+                model[0].save(savepath)
+                modelpath = savepath
+                print('Model successfully saved to {}.'.format(savepath))
+            
+        if action in modes[ACT_DROP]:
+            model = None
+            modelpath = None
+            
+        if action in modes[ACT_CONF]:
+            while True:
+                lines = ["    > o {O:<12}{sep:^3}{V:<15} <".format(O=repr(Opt), sep='-', V=repr(Val)) 
+                                                                  for Opt, Val in config.options.items()]
+                menusize = max(map(len, lines))
+                print(" ")
+                print("    >>> {siz} <<<"
+                        .format(siz='{text:^'+repr(menusize-12)+'}')
+                        .format(text="CONFIGURATION:")
+                    )
+                print("    \n".join(lines))
+                print("    >>> {siz} <<<"
+                        .format(siz='{text:^'+repr(menusize-12)+'}')
+                        .format(text="'Q' - RETURN"))
+                try: option = input('\n    > ')
+                except (KeyboardInterrupt, EOFError): option = 'Q'
+                print(' ')
+                
+                if option == 'Q':
+                    break
+                
+                if option in config.options:
+                    newval = input("    - {} => ".format(option))
+                    if newval:
+                        newval = {'None' : None, 'False' : False, 'True' : True}.get(newval, newval)
+                        config.options[option] = newval
+                        print(" ")
+            
+        if action in modes[DBG_DATA]:
+            _tmp = build_datastreams_gen(*args, xml=config.options.get('xml_path'), 
+                                         txt=config.options.get('txt_path'), dir=config.options.get('directory'), 
+                                         verbose=config.options.get('verbose'), **kwargs)
+            try: _tmp = _tmp[0]
+            except Exception as Err: 
+                print(Err)
+                _tmp = None
+            if _tmp: print(next(_tmp))
+            
+        if action == '!eval':
+            while True:
+                dbgres = None
+                try: query = input('DEBUG: >> ')
+                except (KeyboardInterrupt, EOFError): break
+                if not query or 'q' == query.lower(): break
+                try: dbgres = eval(query)
+                except Exception as E: print('>', E)
+                if dbgres is not None: print(dbgres)
+            
+        if action in modes[ACT_QUIT]:
+            mainloop = False
+    
 if __name__ == '__main__':
-    print('')
+    print(' ')
     import argparse
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--xml')
     argparser.add_argument('--txt')
     argparser.add_argument('--dir')
+    #argparser.add_argument('--mode')
     argparser.add_argument('-v', '--verbose', action='store_true')
     args = vars(argparser.parse_args())
     args['dir'] = args['dir'] or 'mag'
